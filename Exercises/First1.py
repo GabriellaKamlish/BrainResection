b061d7a7142755086b849d06f93cf18e7ee0eda5
Gabriella Kamlish <gabriellakamlish@hotmail.co.uk>
2020-10-21 18:47:27 +0100

    Top or Bottom Task

--- Exercises/First1.py
+++ Exercises/First1.py
@@ -1,19 +1,40 @@
 import SimpleITK as sitk
 import torchio
-import numpy
 from resector.parcellation import get_resectable_hemisphere_mask
 from torchio.datasets import FPG
 import nibabel as nib
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import numpy as np
 
 # fpg data
 FPG_data = FPG()
 # FPG.plot(FPG_data)
 
+# generate resect files using
+# resect /Users/gabriellakamlish/.cache/torchio/fpg/t1.nii.gz /Users/gabriellakamlish/.cache/torchio/fpg/t1_seg_gif.nii.gz t1_resected.nii.gz t1_resection_label.nii.gz
+
+image = np.zeros((256, 256), np.uint8)
+image[50:70, 120:136] = 255
+Image.fromarray(image).save('square.png')
+
+im = Image.open('square.png')
+isize = im.size
+
+result = 'Bottom'
+for x in range(isize[0]//2):
+    for y in range(isize[1]):
+        if image[x,y] == 255:
+            result = 'Top'
+        
+print(result)
+        
+
+
 # 3D Nifti images
-test_image=nib.load('t1_resected.nii.gz').get_fdata()
-test_mask=nib.load('t1_resection_label.nii.gz').get_fdata()
-# (176, 256, 256)
+# test_image=nib.load('t1_resected.nii.gz').get_fdata()
+# test_mask=nib.load('t1_resection_label.nii.gz').get_fdata()
+# # (176, 256, 256)
 
 # fig, (ax1, ax2) = plt.subplots(1,2, figsize = (12, 6))
 # ax1.imshow(test_image)
@@ -22,7 +43,7 @@ test_mask=nib.load('t1_resection_label.nii.gz').get_fdata()
 # ax2.set_title('Mask')
 
 
-# print(torchio.datasets.FPG().seg.path)
+
 # # path to brain parcellation data from fpg
 # parcellation_path = '/tmp/noise/002_S_0295_I118671_t1_pre_NeuroMorph_Parcellation.nii.gz'
 
