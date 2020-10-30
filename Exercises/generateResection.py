fb9422a11db423e7198bb5cf2b9d1d5f6c50b595
Gabriella Kamlish <gabriellakamlish@hotmail.co.uk>
2020-10-30 13:35:27 +0000

    Hemisphere attempt

--- Exercises/generateResection.py
+++ Exercises/generateResection.py
@@ -1,9 +1,17 @@
 import numpy as np
+from PIL import Image
 
 def make_2d_training_instance(mri_path, segmentation_path):
-    
+
+    im = Image.open(segmentation_path)
+    xsize, ysize = im.size
+
+    hemisphere = 'Left'
+    rows, cols = np.where(im == 255)
+    if (cols[cols>(ysize/2)]).size > (cols.size/2):
+        hemisphere = 'Right'
 
 
     slice_png_path = None
-    hemisphere = None
+
     return slice_png_path, hemisphere
